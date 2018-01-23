afd9eb99dcee0b6a62ad404fcec9d0c86633e232
odil-io <github@odil.io>
2018-01-23 12:16:55 +0100

    Fix: padding/margins

--- templates/sections/side_by_side.php
+++ templates/sections/side_by_side.php
@@ -2,20 +2,22 @@
 
 <section id="<?= strtolower( str_replace( ' ','-', get_sub_field('title') ) ); ?>" class="cards bg-light">
 
-	<div class="container py-4">
+  <div class="container">
 
     <?php get_template_part('templates/sections/parts/title'); ?>
 
-
     <div class="row <?= get_sub_field('toggle'); ?>">
-      <div class="col-12 col-md-6">
+
+      <div class="col-12 col-md-6 py-4">
         <?php the_sub_field('content'); ?>
       </div>
+
       <div class="col-12 col-md-6 p-0">
         <?php echo Extras\get_image_tag( get_sub_field('image'), 'large', 'img-fluid'); ?>
       </div>
+
     </div>
 
-	</div>
+  </div>
 
 </section>
